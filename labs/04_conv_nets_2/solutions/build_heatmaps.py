289a8e70e0c3e3e1f14daa49d5eb36341ecf1455
Olivier Grisel <olivier.grisel@ensta.org>
2017-02-23 17:50:53 +0100

    fix w x h ordering

--- labs/04_conv_nets_2/solutions/build_heatmaps.py
+++ labs/04_conv_nets_2/solutions/build_heatmaps.py
@@ -2,13 +2,14 @@ s = "n02084071"
 
 probas_1 = forward_pass_resize("dog.jpg", (400, 640))
 heatmap_1 = build_heatmap(probas_1, synset=s)
+display_img_and_heatmap("dog.jpg", heatmap_1)
 
-probas_2 = forward_pass_resize("dog.jpg", (800,1280))
+probas_2 = forward_pass_resize("dog.jpg", (800, 1280))
 heatmap_2 = build_heatmap(probas_2, synset=s)
+display_img_and_heatmap("dog.jpg", heatmap_2)
 
-probas_3 = forward_pass_resize("dog.jpg", (1600,2560))
+probas_3 = forward_pass_resize("dog.jpg", (1600, 2560))
 heatmap_3 = build_heatmap(probas_3, synset=s)
-
 display_img_and_heatmap("dog.jpg", heatmap_3)
 
 # We observe that heatmap_1 and heatmap_2 gave coarser 
