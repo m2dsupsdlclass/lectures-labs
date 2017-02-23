932ecb37c04afe4908a54c23b30621e3f49910a8
Olivier Grisel <olivier.grisel@ensta.org>
2017-02-23 14:50:43 +0100

    More refinements for the fully conv notebook

--- labs/04_conv_nets_2/solutions/fully_conv.py
+++ labs/04_conv_nets_2/solutions/fully_conv.py
@@ -3,15 +3,17 @@ from keras.models import Model
 
 input = base_model.layers[0].input
 
-# Remove average pooling
+# Take the output of the layer just before the AveragePooling2D
+# layer:
 x = base_model.layers[-2].output
 
-# A 1x1 convolution, with 1000 output channels
+# A 1x1 convolution, with 1000 output channels, one per class
 x = Convolution2D(1000, 1, 1, name='conv1000')(x)
 
-# Softmax on last axis of tensor
+# Softmax on last axis of tensor to normalize the class
+# predictions in each spatial area
 output = SoftmaxMap(axis=-1)(x)
 
-fully_conv_ResNet = Model(input = input, output = output)
+fully_conv_ResNet = Model(input=input, output=output)
 
 # A 1x1 convolution applies a Dense to each spatial grid location
