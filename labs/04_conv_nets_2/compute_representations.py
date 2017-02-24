ec9e65f5ac77ccbe6444f5611c0453e89f7b30b5
Olivier Grisel <olivier.grisel@ensta.org>
2017-02-24 19:16:17 +0100

    WIP: fix the precomputation of the representations with a deterministic ordering

--- labs/04_conv_nets_2/compute_representations.py
+++ labs/04_conv_nets_2/compute_representations.py
@@ -1,39 +1,54 @@
+from __future__ import division
 from keras.applications.resnet50 import ResNet50
 from keras.models import Model
 from keras.applications.imagenet_utils import preprocess_input
 import h5py
 import numpy as np
 from scipy.misc import imread, imresize
-from lxml import etree
+import xml.etree.ElementTree as etree
 import os
 
-annotations = []
 
-# Parse the xml annotation file and retrieve the path to image, its size and annotations
+# Parse the xml annotation file and retrieve the path to each image,
+# its size and annotations
 def extract_xml_annotation(filename):
     z = etree.parse(filename)
-    objects = z.findall("/object")
-    size = (int(z.find("//width").text), int(z.find("//height").text))
-    fname = z.find("/filename").text
-    dics = [{obj.find("name").text:[int(obj.find("bndbox/xmin").text), 
-                                    int(obj.find("bndbox/ymin").text), 
-                                    int(obj.find("bndbox/xmax").text), 
-                                    int(obj.find("bndbox/ymax").text)]} 
-            for obj in objects]
-    output = {"size": size, "filename":fname, "objects":dics}
-    return output
+    objects = z.findall("./object")
+    size = (int(z.find(".//width").text), int(z.find(".//height").text))
+    fname = z.find("./filename").text
+    dicts = [{obj.find("name").text:[int(obj.find("bndbox/xmin").text),
+                                     int(obj.find("bndbox/ymin").text),
+                                     int(obj.find("bndbox/xmax").text),
+                                     int(obj.find("bndbox/ymax").text)]}
+             for obj in objects]
+    return {"size": size, "filename": fname, "objects": dicts}
+
 
 # Filters annotations keeping only those we are interested in
-filters = ["dog", "cat"]
-for file in os.listdir("VOCdevkit/VOC2007/Annotations/"):
-    annotation = extract_xml_annotation("VOCdevkit/VOC2007/Annotations/" +file)
+# We only keep images in which there is a single item
+annotations = []
+
+filters = ["dog", "cat", "bus", "car", "aeroplane"]
+idx2labels = {k: v for k, v in enumerate(filters)}
+labels2idx = {v: k for k, v in idx2labels.items()}
+
+annotation_folder = "VOCdevkit/VOC2007/Annotations/"
+for filename in sorted(os.listdir(annotation_folder)):
+    annotation = extract_xml_annotation(op.join(annotation_folder, filename))
+
     new_objects = []
     for obj in annotation["objects"]:
+        # keep only labels we're interested in
         if list(obj.keys())[0] in filters:
             new_objects.append(obj)
-    if len(new_objects)>0:
-        annotation["objects"] = new_objects
+
+    # Keep only if there's a single object in the image
+    if len(new_objects) == 1:
+        annotation["class"] = list(new_objects[0].keys())[0]
+        annotation["bbox"] = list(new_objects[0].values())[0]
+        annotation.pop("objects")
         annotations.append(annotation)
+
 
 # Predict batch function
 def predict_batch(model, img_batch_path, img_size=None):
@@ -55,22 +70,25 @@ def predict_batch(model, img_batch_path, img_size=None):
     batch = preprocess_input(img_batch)
     return model.predict(x = img_batch)
 
+
 # Build the resnet
 model = ResNet50(include_top=False)
 input = model.layers[0].input
 
+
 # Remove the average pooling layer!
 output = model.layers[-2].output
 headless_conv = Model(input = input, output = output)
+
 
 # Computing representations for all images
 def compute_representations(annotations):
     batch_size = 32
     batches = []
 
-    for a_idx in range(len(annotations)//32+1):
-        batch_bgn = a_idx*32
-        batch_end = min(len(annotations), (a_idx+1)*32)
+    for a_idx in range(len(annotations) // 32 + 1):
+        batch_bgn = a_idx * 32
+        batch_end = min(len(annotations), (a_idx + 1) * 32)
         img_names = []
         for annotation in annotations[batch_bgn:batch_end]:
             img_names.append("VOCdevkit/VOC2007/JPEGImages/" + annotation["filename"])
@@ -79,10 +97,11 @@ def compute_representations(annotations):
         print("batch " +str(a_idx) + " prepared") 
     return np.vstack(batches)
 
+
 # warning this may take some time!
 reprs = compute_representations(annotations)
 
 # Serialize representations
-h5f = h5py.File('voc_representaions.h5', 'w')
+h5f = h5py.File('voc_representations.h5', 'w')
 h5f.create_dataset('reprs', data=reprs)
 h5f.close()
