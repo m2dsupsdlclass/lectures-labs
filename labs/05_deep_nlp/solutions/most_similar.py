c141631eaffe5e1bb671dbd4b1150afd4d8aa749
Olivier Grisel <olivier.grisel@ensta.org>
2017-03-02 15:12:45 +0100

    More small improvements to lab 05

--- labs/05_deep_nlp/solutions/most_similar.py
+++ labs/05_deep_nlp/solutions/most_similar.py
@@ -1,18 +1,18 @@
 def most_similar(words, topn=10):
-    emb = 0
+    query_emb = 0
     # If we have a list of words instead of one word
     # (bonus question)
     if type(words) == list:
         for word in words:
-            emb += get_emb(word)       
+            query_emb += get_emb(word)       
     else:
-        emb = get_emb(words)
+        query_emb = get_emb(words)
         
-    emb = emb / np.linalg.norm(emb)
+    query_emb = query_emb / np.linalg.norm(query_emb)
     
     # Large numpy vector with all cosine similarities
     # between emb and all other words
-    cosines = np.dot(glove_embeddings_norm, emb)
+    cosines = np.dot(glove_embeddings_normed, query_emb)
     
     # topn most similar indexes corresponding to cosines
     idxs = np.argsort(cosines)[::-1][:topn]
