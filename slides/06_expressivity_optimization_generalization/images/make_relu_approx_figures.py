3daf2dcd7f0dfe7aaf04f0b93e1098495d7833b2
Olivier Grisel <olivier.grisel@ensta.org>
2017-03-08 17:31:06 +0100

    rectanbles

---
+++ slides/06_expressivity_optimization_generalization/images/make_relu_approx_figures.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def relu(x):
+    return np.maximum(x, 0)
+
+
+def rect(x, a=1, b=2, h=2, eps=1e-7):
+    return h / eps * (relu(x - a) - relu(x - (a + eps))
+                      - relu(x - b) + relu(x - (b + eps)))
+
+x = np.linspace(-3, 3, 1000)
+y = rect(x, -1, 0, 0.4) + rect(x, 0, 1, 1.3) + rect(x, 1, 2, 0.8)
+plt.plot(x, y)
+
+plt.savefig('three-rectangles.svg')
