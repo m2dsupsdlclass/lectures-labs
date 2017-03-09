45f907716dc424563e49cd98664a557b8d89bae1
Olivier Grisel <olivier.grisel@ensta.org>
2017-03-09 11:33:11 +0100

    output surface

--- slides/06_expressivity_optimization_generalization/images/random_relu_surface.py
+++ slides/06_expressivity_optimization_generalization/images/random_relu_surface.py
@@ -3,7 +3,7 @@ import numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
 
-n_hidden = 50
+n_hidden = 30
 n_layers = 3
 s = 0.01
 
@@ -13,8 +13,8 @@ def weight_init(n_in, n_out):
 
 
 def bias_init(n):
-    return s * np.random.randn(n) / 100
-    # return np.zeros(n)
+    # return s * np.random.randn(n) / 100
+    return np.zeros(n)
 
 
 m = Sequential()
@@ -28,18 +28,22 @@ for l in range(n_layers - 1):
 m.add(Dense(1, weights=[weight_init(n_hidden, 1),
                         bias_init(1)]))
 
-fig = plt.figure(figsize=(12, 10))
-
 # Make data.
-x1 = np.linspace(-.5, .5, 100)
-x2 = np.linspace(-.5, .5, 100)
+n_steps = 200
+x1 = np.linspace(-.5, .5, n_steps)
+x2 = np.linspace(-.5, .5, n_steps)
 X1, X2 = np.meshgrid(x1, x2)
 X = np.vstack([X1.ravel(), X2.ravel()]).T
-Y = m.predict(X, batch_size=256).reshape(100, 100)
-plt.pcolormesh(X1, X2, Y)
+
+# Make predictions on the grid
+Y = m.predict(X, batch_size=256).reshape(n_steps, n_steps)
+w = np.abs(Y).max()
+
+# Plot output surface
+fig = plt.figure(figsize=(12, 10))
+plt.pcolormesh(X1, X2, Y, vmin=-w, vmax=w, cmap=plt.cm.RdBu_r)
 plt.contour(X1, X2, Y, 6, colors='k')
 
+# Histogram of output values
 plt.figure(figsize=(12, 3))
 plt.hist(Y.ravel(), bins=50)
-print(Y.min(), Y.max())
-# plt.imshow(Y)
