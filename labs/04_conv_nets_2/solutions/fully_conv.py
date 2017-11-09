5c4d28d528f105324ff046a3c04526f34ea5f588
collion <charles.ollion@gmail.com>
2017-11-09 11:09:18 +0100

    compatibility changes

--- labs/04_conv_nets_2/solutions/fully_conv.py
+++ labs/04_conv_nets_2/solutions/fully_conv.py
@@ -8,12 +8,12 @@ input = base_model.layers[0].input
 x = base_model.layers[-2].output
 
 # A 1x1 convolution, with 1000 output channels, one per class
-x = Convolution2D(1000, 1, 1, name='conv1000')(x)
+x = Convolution2D(1000, (1, 1), name='conv1000')(x)
 
 # Softmax on last axis of tensor to normalize the class
 # predictions in each spatial area
 output = SoftmaxMap(axis=-1)(x)
 
-fully_conv_ResNet = Model(input=input, output=output)
+fully_conv_ResNet = Model(inputs=input, outputs=output)
 
 # A 1x1 convolution applies a Dense to each spatial grid location
