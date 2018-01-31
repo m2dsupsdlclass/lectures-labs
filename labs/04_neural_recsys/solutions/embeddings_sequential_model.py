8d1d68bae423e644c8d7c42c2dadf17f6b4bf525
Olivier Grisel <olivier.grisel@ensta.org>
2018-01-31 15:10:28 +0100

    Better embeddings notebook

---
+++ labs/04_neural_recsys/solutions/embeddings_sequential_model.py
@@ -0,0 +1,10 @@
+from keras.models import Sequential
+
+
+model3 = Sequential([
+    embedding_layer,
+    Flatten(),
+])
+
+labels_to_encode = np.array([[3]])
+print(model3.predict(labels_to_encode))
