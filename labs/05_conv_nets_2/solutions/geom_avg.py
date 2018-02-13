260bb54d81d1e93437783d099dbbdc8b0b8839f2
collion <charles.ollion@gmail.com>
2018-02-13 19:23:16 +0100

    updated fully conv neural networks

--- labs/05_conv_nets_2/solutions/geom_avg.py
+++ labs/05_conv_nets_2/solutions/geom_avg.py
@@ -1,6 +1,7 @@
-heatmap_1_r = imresize(heatmap_1, (50,80)).astype("float32")
-heatmap_2_r = imresize(heatmap_2, (50,80)).astype("float32")
-heatmap_3_r = imresize(heatmap_3, (50,80)).astype("float32")
+heatmap_1_r = resize(heatmap_1, (50,80), mode='reflect', preserve_range=True)
+heatmap_2_r = resize(heatmap_2, (50,80), mode='reflect', preserve_range=True)
+heatmap_3_r = resize(heatmap_3, (50,80), mode='reflect', preserve_range=True)
+
 
 heatmap_geom_avg = np.power(heatmap_1_r * heatmap_2_r * heatmap_3_r, 0.333)
 display_img_and_heatmap("dog.jpg", heatmap_geom_avg)
