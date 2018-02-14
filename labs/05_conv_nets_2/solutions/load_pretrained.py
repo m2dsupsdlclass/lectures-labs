8c2a2beb5c6d8f46b6bffcb40740e08869bbb2e0
collion <charles.ollion@gmail.com>
2018-02-14 20:41:30 +0100

    updated notebook2

--- labs/05_conv_nets_2/solutions/load_pretrained.py
+++ labs/05_conv_nets_2/solutions/load_pretrained.py
@@ -6,4 +6,4 @@ input = model.layers[0].input
 
 # Remove the average pooling layer
 output = model.layers[-2].output
-headless_conv = Model(input=input, output=output)
+headless_conv = Model(inputs=input, outputs=output)
