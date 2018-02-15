0b7e127e441e964f1a020506395a0b8d5b29a14d
Olivier Grisel <olivier.grisel@ensta.org>
2018-02-15 22:09:42 +0100

    More improvements

--- labs/05_conv_nets_2/solutions/classif_and_loc.py
+++ labs/05_conv_nets_2/solutions/classif_and_loc.py
@@ -3,20 +3,30 @@
 # of input data forbids us to build much deeper networks
 
 def classif_and_loc(num_classes):
-    model_input = Input(shape=(7,7,2048))
-    x = GlobalAveragePooling2D()(model_input)
+    model_input = Input(shape=(7, 7, 2048))
+    # For single object classification, the exact spatial information
+    # held in the feature map is not very important. It's fine to perform
+    # global average pooling of the features at the beginning of the
+    # classification tower of the model.
+    classification_tower = GlobalAveragePooling2D()(model_input)
     
-    x = Dropout(0.2)(x)
-    head_classes = Dense(num_classes, activation="softmax", name="head_classes")(x)
+    classification_tower = Dropout(0.2)(classification_tower)
+    head_classes = Dense(num_classes, activation="softmax",
+                         name="head_classes")(classification_tower)
     
-    y = Convolution2D(4, (1, 1), activation='relu', name='hidden_conv')(model_input)
-    y = Flatten()(y)
-    y = Dropout(0.2)(y)
-    head_boxes = Dense(4, name="head_boxes")(y)
+    # We do not use global average pooling in the localization tower of the
+    # model so as to preserve coarse grained 7 x 7 spatial information
+    # from the last convolutional feature maps computed by the ResNet50
+    # model.
+    localization_tower = Convolution2D(4, (1, 1), activation='relu',
+                                       name='hidden_conv')(model_input)
+    localization_tower = Flatten()(localization_tower)
+    localization_tower = Dropout(0.2)(localization_tower)
+    head_boxes = Dense(4, name="head_boxes")(localization_tower)
     
     model = Model(model_input, outputs = [head_classes, head_boxes], name="resnet_loc")
     model.compile(optimizer="adam", loss=['categorical_crossentropy', "mse"], 
-                  loss_weights=[1., 1/(224*224)]) 
+                  loss_weights=[1., 1 / (224 * 224)]) 
     return model
 
 
