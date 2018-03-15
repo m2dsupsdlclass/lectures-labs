6a6e5e67c21da283055cb3270d35bd1bccbf8e8a
Olivier Grisel <olivier.grisel@ensta.org>
2018-03-15 23:14:16 +0100

    Improvements to siamese notebook

--- labs/09_triplet_loss/solutions/most_similar.py
+++ labs/09_triplet_loss/solutions/most_similar.py
@@ -1,7 +1,23 @@
+# Optional: add some test images
+more_img_paths = {
+    'olivier': [os.path.join('test_images/olivier', img)
+                for img in sorted(os.listdir('test_images/olivier'))],
+    'charles': [os.path.join('test_images/charles', img)
+                for img in sorted(os.listdir('test_images/charles'))],
+}
+img_paths.update(more_img_paths)
+all_images_path = []
+for img_list in img_paths.values():
+    all_images_path += img_list
+path_to_id = {v: k for k, v in enumerate(all_images_path)}
+id_to_path = {v: k for k, v in path_to_id.items()}
+all_imgs = open_all_images(id_to_path)
+
+# Actually compute the similarities
 emb = shared_conv.predict(all_imgs)
 emb = emb / np.linalg.norm(emb, axis=-1, keepdims=True)
 
-def most_sim(x, emb, topn=5):
-    sims = np.dot(emb,x)
+def most_sim(x, emb, topn=4):
+    sims = np.dot(emb, x)
     ids = np.argsort(sims)[::-1]
-    return [(id,sims[id]) for id in ids[:topn]]
+    return [(id, sims[id]) for id in ids[:topn]]
--- labs/09_triplet_loss/solutions/shared_conv.py
+++ labs/09_triplet_loss/solutions/shared_conv.py
@@ -1,4 +1,4 @@
-inp = Input((60,60,3), dtype='float32')
+inp = Input((60, 60, 3), dtype='float32')
 x = Conv2D(16, 3, activation="relu", padding="same")(inp)
 x = Conv2D(16, 3, activation="relu", padding="same")(x)
 x = MaxPool2D((2,2))(x) # 30,30
@@ -13,4 +13,4 @@ x = Conv2D(32, 3, activation="relu", padding="same")(x)
 x = Flatten()(x)
 x = Dropout(0.2)(x)
 x = Dense(50)(x)
-shared_conv = Model(inputs=inp, outputs = x)
+shared_conv = Model(inputs=inp, outputs=x)
