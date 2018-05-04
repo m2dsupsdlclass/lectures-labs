a32c323c53e90d7bb10cea62c821800392e9c1dc
deepPhong <dinh.nguyen@polytechnique.edu>
2018-05-04 18:56:43 +0200

    Updated first notebook and solutions to PyTorch 0.4.0 API changes and recommendations

--- labs/08_frameworks/solutions/cosine_autograd.py
+++ labs/08_frameworks/solutions/cosine_autograd.py
@@ -4,8 +4,8 @@ def g(x, y):
     return xy / norms
 
 
-x = Variable(torch.FloatTensor([0, 1, 2]), requires_grad=True)
-y = Variable(torch.FloatTensor([3, 0.9, 2.2]), requires_grad=True)
+x = torch.tensor([0, 1, 2], dtype=torch.float32, requires_grad=True)
+y = torch.tensor([3, 0.9, 2.2], dtype=torch.float32, requires_grad=True)
 
 cosine = g(x, y)
 cosine.backward()
