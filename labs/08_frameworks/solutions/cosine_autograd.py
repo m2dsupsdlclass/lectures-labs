b946c5bdc092845ebdfe69839289bce669e5a4bb
Olivier Grisel <olivier.grisel@ensta.org>
2018-05-07 15:21:59 +0200

    Intro to pytorch: upgrade to 0.4.0

--- labs/08_frameworks/solutions/cosine_autograd.py
+++ labs/08_frameworks/solutions/cosine_autograd.py
@@ -8,5 +8,8 @@ x = torch.tensor([0, 1, 2], dtype=torch.float32, requires_grad=True)
 y = torch.tensor([3, 0.9, 2.2], dtype=torch.float32, requires_grad=True)
 
 cosine = g(x, y)
+print("cosine: ", cosine)
+
 cosine.backward()
-x.grad, y.grad
+print('x.grad:', x.grad)
+print('y.grad:', y.grad)
--- labs/08_frameworks/solutions/cosine_autograd_colinear.py
+++ labs/08_frameworks/solutions/cosine_autograd_colinear.py
@@ -2,5 +2,12 @@ x = torch.tensor([0, 1, 2], dtype=torch.float32, requires_grad=True)
 y = torch.tensor(2 * x.data.clone(), requires_grad=True)
 
 cosine = g(x, y)
+print("cosine: ", cosine)
+
 cosine.backward()
-x.grad, y.grad
+print('x.grad:', x.grad)
+print('y.grad:', y.grad)
+
+# The gradient of the cosine similarity of colinear vectors is
+# null w.r.t. each of the two vector as the cosine function
+# is already at its global maximum with a value of 1.0.
