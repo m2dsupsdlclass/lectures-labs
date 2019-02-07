07865610c4a44d827570949f06806cef3fbc4754
Olivier Grisel <olivier.grisel@ensta.org>
2019-02-07 23:31:47 +0100

    Fix missing import in solution

--- labs/05_conv_nets_2/solutions/geom_avg.py
+++ labs/05_conv_nets_2/solutions/geom_avg.py
@@ -1,6 +1,8 @@
-heatmap_1_r = resize(heatmap_1, (50,80), mode='reflect', preserve_range=True)
-heatmap_2_r = resize(heatmap_2, (50,80), mode='reflect', preserve_range=True)
-heatmap_3_r = resize(heatmap_3, (50,80), mode='reflect', preserve_range=True)
+from skimage.transform import resize
+
+heatmap_1_r = resize(heatmap_1, (50,80), mode='reflect', preserve_range=True, anti_aliasing=True)
+heatmap_2_r = resize(heatmap_2, (50,80), mode='reflect', preserve_range=True, anti_aliasing=True)
+heatmap_3_r = resize(heatmap_3, (50,80), mode='reflect', preserve_range=True, anti_aliasing=True)
 
 
 heatmap_geom_avg = np.power(heatmap_1_r * heatmap_2_r * heatmap_3_r, 0.333)
