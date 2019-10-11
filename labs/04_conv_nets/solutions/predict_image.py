65c306df138cdf05f403545cc2acf025b340136a
collion <charles.ollion@gmail.com>
2019-10-11 10:40:02 +0200

    update conv pretrained for tf2.0

--- labs/04_conv_nets/solutions/predict_image.py
+++ labs/04_conv_nets/solutions/predict_image.py
@@ -1,7 +1,7 @@
 from skimage.io import imread
 from skimage.transform import resize
-from keras.applications.imagenet_utils import preprocess_input
-from keras.applications.imagenet_utils import decode_predictions
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+from tensorflow.keras.applications.imagenet_utils import decode_predictions
 
 path = "images_resize/000007.jpg"
 
@@ -11,7 +11,7 @@ plt.imshow(img)
 img = resize(img, (224, 224), mode='reflect', preserve_range=True,
              anti_aliasing=True)
 # add a dimension for a "batch" of 1 image
-img_batch = preprocess_input(img[np.newaxis]) 
+img_batch = preprocess_input(img[np.newaxis])
 
 predictions = model.predict(img_batch)
 decoded_predictions= decode_predictions(predictions)
