ca64f80b26257705862fe4779d10e0a2819e7d59
Arthur Douillard <arthur.douillard@heuritech.com>
2020-01-09 15:16:01 +0000

    Lab03 (#20)

--- labs/03_neural_recsys/solutions/classification.py
+++ labs/03_neural_recsys/solutions/classification.py
@@ -1,39 +1,45 @@
-# For each sample we input the integer identifiers
-# of a single user and a single item
-user_id_input = Input(shape=[1], name='user')
-item_id_input = Input(shape=[1], name='item')
+class ClassificationModel(Model):
+    def __init__(self, embedding_size, max_user_id, max_item_id):
+        super().__init__()
 
-embedding_size = 16
-dense_size = 128
-dropout_embedding = 0.5
-dropout_hidden = 0.2
+        self.user_embedding = Embedding(output_dim=embedding_size, input_dim=max_user_id + 1,
+                                        input_length=1, name='user_embedding')
+        self.item_embedding = Embedding(output_dim=embedding_size, input_dim=max_item_id + 1,
+                                        input_length=1, name='item_embedding')
 
-user_embedding = Embedding(output_dim=embedding_size, input_dim=max_user_id + 1,
-                           input_length=1, name='user_embedding')(user_id_input)
-item_embedding = Embedding(output_dim=embedding_size, input_dim=max_item_id + 1,
-                           input_length=1, name='item_embedding')(item_id_input)
+        # The following two layers don't have parameters.
+        self.flatten = Flatten()
+        self.concat = Concatenate()
 
-# reshape from shape: (batch_size, input_length, embedding_size)
-# to shape: (batch_size, input_length * embedding_size) which is
-# equal to shape: (batch_size, embedding_size)
-user_vecs = Flatten()(user_embedding)
-item_vecs = Flatten()(item_embedding)
+        self.dropout1 = Dropout(0.5)
+        self.dense1 = Dense(128, activation="relu")
+        self.dropout2 = Dropout(0.2)
+        self.dense2 = Dense(128, activation='relu')
+        self.dense3 = Dense(5, activation="softmax")
 
-input_vecs = Concatenate()([user_vecs, item_vecs])
-input_vecs = Dropout(dropout_embedding)(input_vecs)
+    def call(self, inputs):
+        user_inputs = inputs[0]
+        item_inputs = inputs[1]
 
-x = Dense(dense_size, activation='relu')(input_vecs)
-x = Dropout(dropout_hidden)(x)
-x = Dense(dense_size, activation='relu')(x)
-y = Dense(5, activation='softmax')(x)
+        user_vecs = self.flatten(self.user_embedding(user_inputs))
+        item_vecs = self.flatten(self.item_embedding(item_inputs))
 
-model = Model(inputs=[user_id_input, item_id_input], outputs=y)
+        input_vecs = self.concat([user_vecs, item_vecs])
+
+        y = self.dropout1(input_vecs)
+        y = self.dense1(y)
+        y = self.dropout2(y)
+        y = self.dense2(y)
+        y = self.dense3(y)
+
+        return y
+
+model = ClassificationModel(16, max_user_id, max_item_id)
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
 initial_train_preds = model.predict([user_id_train, item_id_train]).argmax(axis=1) + 1
 print("Random init MSE: %0.3f" % mean_squared_error(initial_train_preds, rating_train))
 print("Random init MAE: %0.3f" % mean_absolute_error(initial_train_preds, rating_train))
-
 
 history = model.fit([user_id_train, item_id_train], rating_train - 1,
                     batch_size=64, epochs=15, validation_split=0.1,
--- labs/03_neural_recsys/solutions/deep_explicit_feedback_recsys.py
+++ labs/03_neural_recsys/solutions/deep_explicit_feedback_recsys.py
@@ -1,32 +1,52 @@
 # For each sample we input the integer identifiers
 # of a single user and a single item
-user_id_input = Input(shape=[1], name='user')
-item_id_input = Input(shape=[1], name='item')
+class DeepRegressionModel(Model):
 
-embedding_size = 30
-user_embedding = Embedding(output_dim=embedding_size, input_dim=max_user_id + 1,
-                           input_length=1, name='user_embedding')(user_id_input)
-item_embedding = Embedding(output_dim=embedding_size, input_dim=max_item_id + 1,
-                           input_length=1, name='item_embedding')(item_id_input)
+    def __init__(self, embedding_size, max_user_id, max_item_id):
+        super().__init__()
 
-# reshape from shape: (batch_size, input_length, embedding_size)
-# to shape: (batch_size, input_length * embedding_size) which is
-# equal to shape: (batch_size, embedding_size)
-user_vecs = Flatten()(user_embedding)
-item_vecs = Flatten()(item_embedding)
+        self.user_embedding = Embedding(
+            output_dim=embedding_size,
+            input_dim=max_user_id + 1,
+            input_length=1,
+            name='user_embedding'
+        )
+        self.item_embedding = Embedding(
+            output_dim=embedding_size,
+            input_dim=max_item_id + 1,
+            input_length=1,
+            name='item_embedding'
+        )
 
-input_vecs = Concatenate()([user_vecs, item_vecs])
-## Error 1: Dropout was too high, preventing any training
-input_vecs = Dropout(0.5)(input_vecs)
+        # The following two layers don't have parameters.
+        self.flatten = Flatten()
+        self.concat = Concatenate()
 
-x = Dense(64, activation='relu')(input_vecs)
+        ## Error 1: Dropout was too high, preventing any training
+        self.dropout = Dropout(0.5)
+        self.dense1 = Dense(64, activation="relu")
+        ## Error 2: output dimension was 2 where we predict only 1-d rating
+        ## Error 3: tanh activation squashes the outputs between -1 and 1
+        ## when we want to predict values between 1 and 5
+        self.dense2 = Dense(1)
 
-## Error 2: output dimension was 2 where we predict only 1-d rating
-## Error 3: tanh activation squashes the outputs between -1 and 1
-## when we want to predict values between 1 and 5
-y = Dense(1)(x)
+    def call(self, inputs):
+        user_inputs = inputs[0]
+        item_inputs = inputs[1]
 
-model = Model(inputs=[user_id_input, item_id_input], outputs=y)
+        user_vecs = self.flatten(self.user_embedding(user_inputs))
+        item_vecs = self.flatten(self.item_embedding(item_inputs))
+
+        input_vecs = self.concat([user_vecs, item_vecs])
+
+        y = self.dropout(input_vecs)
+        y = self.dense1(y)
+        y = self.dense2(y)
+
+        return y
+
+
+model = DeepRegressionModel(30, max_user_id, max_item_id)
 ## Error 4: A binary crossentropy loss is only useful for binary
 ## classification, while we are in regression (use mse or mae)
 model.compile(optimizer='adam', loss='mae')
