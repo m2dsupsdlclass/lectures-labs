f7eb602a92ffbefcec4e32d8e96f6bb43b00b422
Olivier Grisel <olivier.grisel@ensta.org>
2020-01-19 23:44:23 +0100

    Formatting fixes to avoid horizontal scroll on videoprojector

--- labs/03_neural_recsys/solutions/deep_implicit_feedback_recsys.py
+++ labs/03_neural_recsys/solutions/deep_implicit_feedback_recsys.py
@@ -1,24 +1,30 @@
-from tensorflow.keras.layers import (Concatenate, Dense, Dropout, Embedding,
-                                     Flatten)
 from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Embedding, Flatten, Dense
+from tensorflow.keras.layers import Concatenate, Dropout
 from tensorflow.keras.regularizers import l2
 
 
 class MLP(layers.Layer):
-    def __init__(self, n_hidden=1, hidden_size=64, dropout=0., l2_reg=None):
+    def __init__(self, n_hidden=1, hidden_size=64, dropout=0.,
+                 l2_reg=None):
         super().__init__()
 
         self.layers = [Dropout(dropout)]
 
         for _ in range(n_hidden):
-            self.layers.append(Dense(hidden_size, activation="relu", kernel_regularizer=l2_reg))
+            self.layers.append(Dense(hidden_size, activation="relu",
+                                     kernel_regularizer=l2_reg))
             self.layers.append(Dropout(dropout))
 
-        self.layers.append(Dense(1, activation="relu", kernel_regularizer=l2_reg))
+        self.layers.append(Dense(1, activation="relu",
+                                 kernel_regularizer=l2_reg))
 
-    def call(self, x):
+    def call(self, x, training=False):
         for layer in self.layers:
-            x = layer(x)
+            if isinstance(layer, Dropout):
+                x = layer(x, training=training)
+            else:
+                x = layer(x)
         return x
 
 
@@ -29,10 +35,15 @@ class DeepTripletModel(Model):
 
         l2_reg = None if l2_reg == 0 else l2(l2_reg)
 
-        self.user_layer = Embedding(n_users, user_dim, input_length=1, input_shape=(1,),
-                                    name='user_embedding', embeddings_regularizer=l2_reg)
-        self.item_layer = Embedding(n_items, item_dim, input_length=1,
-                                    name="item_embedding", embeddings_regularizer=l2_reg)
+        self.user_layer = Embedding(n_users, user_dim,
+                                    input_length=1,
+                                    input_shape=(1,),
+                                    name='user_embedding',
+                                    embeddings_regularizer=l2_reg)
+        self.item_layer = Embedding(n_items, item_dim,
+                                    input_length=1,
+                                    name="item_embedding",
+                                    embeddings_regularizer=l2_reg)
 
         self.flatten = Flatten()
         self.concat = Concatenate()
@@ -46,13 +57,18 @@ class DeepTripletModel(Model):
         pos_item_input = inputs[1]
         neg_item_input = inputs[2]
 
-        user_embedding = self.flatten(self.user_layer(user_input))
-        pos_item_embedding = self.flatten(self.item_layer(pos_item_input))
-        neg_item_embedding = self.flatten(self.item_layer(neg_item_input))
+        user_embedding = self.user_layer(user_input)
+        user_embedding = self.flatten(user_embedding)
+        pos_item_embedding = self.item_layer(pos_item_input)
+        pos_item_embedding = self.flatten(pos_item_embedding)
+        neg_item_embedding = self.item_layer(neg_item_input)
+        neg_item_embedding = self.flatten(neg_item_embedding)
 
         # Similarity computation between embeddings
-        pos_embeddings_pair = self.concat([user_embedding, pos_item_embedding])
-        neg_embeddings_pair = self.concat([user_embedding, neg_item_embedding])
+        pos_embeddings_pair = self.concat([user_embedding,
+                                           pos_item_embedding])
+        neg_embeddings_pair = self.concat([user_embedding,
+                                           neg_item_embedding])
 
         pos_similarity = self.mlp(pos_embeddings_pair)
         neg_similarity = self.mlp(neg_embeddings_pair)
@@ -90,15 +106,18 @@ hyper_parameters = dict(
     n_hidden=1,
     hidden_size=128,
     dropout=0.1,
-    l2_reg=0.
+    l2_reg=0.,
 )
-deep_triplet_model = DeepTripletModel(n_users, n_items, **hyper_parameters)
-deep_match_model = DeepMatchModel(deep_triplet_model.user_layer, deep_triplet_model.item_layer, deep_triplet_model.mlp)
+deep_triplet_model = DeepTripletModel(n_users, n_items,
+                                      **hyper_parameters)
+deep_match_model = DeepMatchModel(deep_triplet_model.user_layer,
+                                  deep_triplet_model.item_layer,
+                                  deep_triplet_model.mlp)
 
 deep_triplet_model.compile(loss=identity_loss, optimizer='adam')
 fake_y = np.ones_like(pos_data_train['user_id'])
 
-n_epochs = 15
+n_epochs = 20
 
 for i in range(n_epochs):
     # Sample new negatives to build different triplets at each epoch
@@ -110,7 +129,9 @@ for i in range(n_epochs):
     deep_triplet_model.fit(triplet_inputs, fake_y, shuffle=True,
                            batch_size=64, epochs=1)
 
-    # Monitor the convergence of the model
-    test_auc = average_roc_auc(deep_match_model, pos_data_train, pos_data_test)
-    print("Epoch %d/%d: test ROC AUC: %0.4f"
-          % (i + 1, n_epochs, test_auc))
+
+# Monitor the convergence of the model
+test_auc = average_roc_auc(
+    deep_match_model, pos_data_train, pos_data_test)
+print("Epoch %d/%d: test ROC AUC: %0.4f"
+      % (i + 1, n_epochs, test_auc))
