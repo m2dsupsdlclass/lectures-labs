a9478888a173c76ff4e5e2226aa5a1cd23090283
Olivier Grisel <olivier.grisel@ensta.org>
2022-01-20 15:21:18 +0100

    Fix single-sample array passed to inverse_transform

--- labs/01_keras/solutions/keras_accuracy_on_test_set.py
+++ labs/01_keras/solutions/keras_accuracy_on_test_set.py
@@ -3,7 +3,7 @@ y_predicted = np.argmax(model.predict(X_test, verbose=0), axis=1)
 # Let's display the first inputs image, the predicted labels and the true labels
 fig, axes = plt.subplots(ncols=5, nrows=3, figsize=(12, 9))
 for i, ax in enumerate(axes.ravel()):
-    ax.imshow(scaler.inverse_transform(X_test[i]).reshape(8, 8),
+    ax.imshow(scaler.inverse_transform(X_test[i:i+1]).reshape(8, 8),
               cmap=plt.cm.gray_r, interpolation='nearest')
     ax.set_title("predicted label: %d\n true label: %d"
                  % (y_predicted[i], y_test[i]))
